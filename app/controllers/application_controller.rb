56eb1a125c1f69e8d15bfed8256f71a47b68a137
KonstantinStanmeyer <konstantin@stanmeyer.com>
2022-10-03 20:51:28 -0600

    reformatting the controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,41 +1,71 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
+  #DISHES
+
+  #GET all dishes fid all restaurants for all delivery services
+
   get "/dishes" do
     dishes = Dish.all
     dishes.to_json
   end
+
+  #GET a specific dish from its id
 
   get "/dishes/:id" do
     dish = Dish.find(params[:id])
     dish.to_json
   end
 
-  get "/restaurants" do
-    restaurants = Restaurant.all
-    restaurants.to_json
-  end
+  #DELIVERY
 
-  get "/restaurants/:id" do
-    restaurant = Restaurant.find(params[:id])
-    restaurant.to_json
-  end
+  #GET all delivery services
 
   get "/delivery" do
     deliveries = Delivery.all
     deliveries.to_json
   end
 
+  #GET a specific delivery service from its id
+
   get "/delivery/:id" do
     delivery = Delivery.find(params[:id])
     delivery.to_json
   end
 
+  #GET all restaurants for a specific delivery service
+
+  get "/delivery/:id/restaurants" do
+    restaurants = Delivery.find(params[:id]).restaurants
+    restaurants.to_json
+  end
+
+  #RESTAURANTS
+
+  #GET all restaurants for all delivery services
+
+  get "/restaurants" do
+    restaurants = Restaurant.all
+    restaurants.to_json
+  end
+
+  #GET a specific restaurant from its id
+
+  get "/restaurants/:id" do
+    restaurant = Restaurant.find(params[:id])
+    restaurant.to_json
+  end
+
+  #GET all ratings for a specific restaurant from its id
+
   get "/restaurants/:id/ratings" do
     ratings = Restaurant.find(params[:id]).ratings
     ratings.to_json
   end
+
+  #RATINGS
+
+  #GET all ratings for all restaurants for all delivery services
 
   get "/ratings" do
     ratings = Rating.all
